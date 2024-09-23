57492f1ce83c848df3f51d2efdfc8eeaa1f814ae
Byron L <byronlitle05@gmail.com>
2024-09-22 19:44:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5_E3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingCart cart = new ShoppingCart();
+            int selectChoice = 0;
+            string userInput = "";
+            Console.WriteLine("Welcome to the store!");
+            Console.WriteLine();
+            //Code that outputs all products in list collection?
+            do
+            {
+                Console.WriteLine("Press 1 to add more product(s), press 2 to remove a product, press 3 to calculate price. To exit the program, press 4.");
+                selectChoice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (selectChoice)
+                {
+                    case 1:
+                        Console.WriteLine("What would you like to get?");
+                        userInput = Console.ReadLine();
+                        cart.AddProduct(userInput);
+                        break;
+                    case 2:
+                        Console.WriteLine("What would you like to remove?");
+                        userInput = Console.ReadLine();
+                        cart.RemoveProduct(userInput);
+                        break;
+                    case 3:
+                        Console.WriteLine($"Your total comes out to: {cart.CalculateTotalPrice()}");
+                        break;
+                    case 4:
+                        break;
+                }
+            } while (selectChoice != 4);
+        }
+        class Product
+        {
+            public string ProductID = "";
+            public string ProductName = "";
+            public double Price = 0;
+        }
+        class ShoppingCart
+        {
+            List<Product> Items = new List<Product>()
+            {
+                new Product(){ProductID = "A1", ProductName="Apple", Price=1.50},
+                new Product(){ProductID = "A2", ProductName="Orange", Price=2.00},
+                new Product(){ProductID = "B7", ProductName="Vacuum", Price=69.99},
+                new Product(){ProductID = "C5", ProductName="Chips", Price=3.00},
+                new Product(){ProductID = "D4", ProductName="Garden Hose", Price=19.99},
+            };
+                
+            public void AddProduct(string product)
+            {
+                //Compare userInput to ProductName; add to cart after comparison
+                Items.Add(product);
+            }
+            public void RemoveProduct(string productID) 
+            {
+                //Display what is in cart + productID 
+                Items.Remove(productID);
+            }
+            public double CalculateTotalPrice()
+            {
+                double totalPrice = 0;
+                //Grab price from list; add them; and times by 8.25%(1.0825) to include taxes
+                return totalPrice;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("W5_E3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("W5_E3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ff6b900-1222-4530-aff8-7eaf6456dcc8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
